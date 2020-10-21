c9bbb25c5f6794c2aaab9f55d843c408a9d5f776
ts1tua <vivik32@bk.ru>
2020-10-22 01:25:51 +0300

    c

---
+++ 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.Design.Serialization;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace _1
+{
+    class Program
+    {
+        static void  Main(string[] args)
+        {
+            Console.WriteLine("Hello World from 1!");
+        }
+    }
+}
